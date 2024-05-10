4fd01e707dc13454011e82f1236f8e52e6e4d2e3
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-10 18:06:36 +0200

    update processVoice agentReply

--- src/app/api/processVoice/route.ts
+++ src/app/api/processVoice/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
 
   const result: ProcessVoiceResponse = {
     action: isItARequest ? { addingTodo: `${input}` } : { retriveTodos: true },
-    agentReply: isItARequest ? "Let me see." : "Ok, done.",
+    agentReply: isItARequest ? "Let me see." : "Ok.",
     payload: { isItARequest },
   };
 
