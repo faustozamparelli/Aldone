cbf570140a0455797574bd3a0ec5f6fcb15a1658
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-10 17:56:41 +0200

    check wheter each question is a request

--- src/app/api/processVoice/route.ts
+++ src/app/api/processVoice/route.ts
@@ -4,23 +4,35 @@ export interface ProcessVoiceRequest {
   input: string[];
 }
 
-interface VoiceAction {
+export interface VoiceAction {
   addingTodo?: string;
   completingTodoIndex?: number;
-  removingTodoIndex?: number;
+  retriveTodos?: boolean;
 }
 
 export interface ProcessVoiceResponse {
   action: VoiceAction;
   agentReply: string;
+  payload?: any;
 }
 
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const { input }: ProcessVoiceRequest = await req.json();
 
+  const isItARequest = (
+    await (
+      await fetch("http://localhost:3001/question_classifier", {
+        method: "POST",
+        body: JSON.stringify({ text: input }),
+        headers: { "Content-Type": "application/json" },
+      })
+    ).json()
+  )["is_it_a_question"];
+
   const result: ProcessVoiceResponse = {
-    action: { addingTodo: "To-do processed correctly" },
-    agentReply: "Ok, done.",
+    action: isItARequest ? { addingTodo: `${input}` } : { retriveTodos: true },
+    agentReply: isItARequest ? "Let me see." : "Ok, done.",
+    payload: { isItARequest },
   };
 
   return NextResponse.json(result);
