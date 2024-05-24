882be61e9d978c70da245fcf5cc9b309569bbe4e
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-24 16:17:52 +0200

    move prof code to example.py

--- src/py/todo_classifier/example.py
+++ src/py/todo_classifier/example.py
@@ -1,4 +1,3 @@
-# import the dataset and the modules
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -7,23 +6,17 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# get a subset of categories
 categories = ["talk.religion.misc", "sci.space", "comp.graphics"]
 
-# load the dataset
 train = fetch_20newsgroups(subset="train", categories=categories)
 test = fetch_20newsgroups(subset="test", categories=categories)
 
-# prepare the data and create the model
 model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
-# train the model
 model.fit(train.data, train.target)
 
-# test the model
 labels = model.predict(test.data)
 
-# plot the confusion matrix
 conf_matrix = confusion_matrix(test.target, labels)
 sns.heatmap(
     conf_matrix.T,
@@ -38,7 +31,6 @@ plt.xlabel("true labels")
 plt.ylabel("predicted labels")
 plt.show()
 
-# try the classifier with our text
 s = "I believe in the future"
 pred = model.predict([s])
 print(train.target_names[pred[0]])
