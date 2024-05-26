9a77080db514377edb25ade347bfcab354bc9697
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-26 13:57:32 +0200

    use yolo classes

--- src/py/grocery_recognition/grocery_recognition.py
+++ src/py/grocery_recognition/grocery_recognition.py
@@ -8,9 +8,54 @@ cap.set(4, 480)
 
 model = YOLO("yolo-Weights/yolov8n.pt")
 
+
 classNames = [
+    "person",
+    "bicycle",
+    "car",
+    "motorbike",
+    "aeroplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
     "bottle",
     "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
     "banana",
     "apple",
     "sandwich",
@@ -21,6 +66,30 @@ classNames = [
     "pizza",
     "donut",
     "cake",
+    "chair",
+    "sofa",
+    "pottedplant",
+    "bed",
+    "diningtable",
+    "toilet",
+    "tvmonitor",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
 ]
 
 
@@ -28,14 +97,12 @@ while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
@@ -43,25 +110,26 @@ while True:
             print("Confidence --->", confidence)
 
             cls = int(box.cls[0])
+            print({"cls": cls})
             if cls in range(len(classNames)):
                 print("Class name -->", classNames[cls])
 
-            # object details
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
             color = (255, 0, 0)
             thickness = 2
 
-            cv2.putText(
-                img,
-                classNames[cls] if cls in range(len(classNames)) else cls,
-                org,
-                font,
-                fontScale,
-                color,
-                thickness,
-            )
+            if cls in range(len(classNames)):
+                cv2.putText(
+                    img,
+                    classNames[cls],
+                    org,
+                    font,
+                    fontScale,
+                    color,
+                    thickness,
+                )
 
     cv2.imshow("Webcam", img)
     if cv2.waitKey(1) == ord("q"):
