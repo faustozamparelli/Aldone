464c1a92f88f550b5df5a0a701e5f76e34d6abb1
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-26 14:13:23 +0200

    simplify yolo code

--- src/py/grocery_recognition/grocery_recognition.py
+++ src/py/grocery_recognition/grocery_recognition.py
@@ -9,89 +9,6 @@ cap.set(4, 480)
 model = YOLO("yolo-Weights/yolov8n.pt")
 
 
-classNames = [
-    "person",
-    "bicycle",
-    "car",
-    "motorbike",
-    "aeroplane",
-    "bus",
-    "train",
-    "truck",
-    "boat",
-    "traffic light",
-    "fire hydrant",
-    "stop sign",
-    "parking meter",
-    "bench",
-    "bird",
-    "cat",
-    "dog",
-    "horse",
-    "sheep",
-    "cow",
-    "elephant",
-    "bear",
-    "zebra",
-    "giraffe",
-    "backpack",
-    "umbrella",
-    "handbag",
-    "tie",
-    "suitcase",
-    "frisbee",
-    "skis",
-    "snowboard",
-    "sports ball",
-    "kite",
-    "baseball bat",
-    "baseball glove",
-    "skateboard",
-    "surfboard",
-    "tennis racket",
-    "bottle",
-    "wine glass",
-    "cup",
-    "fork",
-    "knife",
-    "spoon",
-    "bowl",
-    "banana",
-    "apple",
-    "sandwich",
-    "orange",
-    "broccoli",
-    "carrot",
-    "hot dog",
-    "pizza",
-    "donut",
-    "cake",
-    "chair",
-    "sofa",
-    "pottedplant",
-    "bed",
-    "diningtable",
-    "toilet",
-    "tvmonitor",
-    "laptop",
-    "mouse",
-    "remote",
-    "keyboard",
-    "cell phone",
-    "microwave",
-    "oven",
-    "toaster",
-    "sink",
-    "refrigerator",
-    "book",
-    "clock",
-    "vase",
-    "scissors",
-    "teddy bear",
-    "hair drier",
-    "toothbrush",
-]
-
 foods = {
     "banana",
     "apple",
@@ -113,30 +30,14 @@ while True:
     for r in results:
         boxes = r.boxes
         for box in boxes:
-            cls = int(box.cls[0])
-            if cls in range(len(classNames)) and classNames[cls] in foods:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-                confidence = math.ceil((box.conf[0] * 100)) / 100
-
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
-
-                cv2.putText(
-                    img,
-                    classNames[cls] + " " + str(confidence) + "%",
-                    org,
-                    font,
-                    fontScale,
-                    color,
-                    thickness,
-                )
+            cls = r.names[int(box.cls[0])]
+            if cls not in foods:
+                break
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            cv2.putText(img, cls, [x1, y1], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     cv2.imshow("Webcam", img)
     if cv2.waitKey(1) == ord("q"):
