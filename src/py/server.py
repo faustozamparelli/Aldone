5a9a2fc5544a5e399dd3ac1edb30a77b05b78719
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-05-31 20:34:37 +0200

    implement python server todo_classifier route

--- src/py/server.py
+++ src/py/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from question_classifier.nnlabler import TextClassifier, is_it_a_query
+from todo_classifier.use_classifier import classify_todo
+import json
 
 app = Flask(__name__)
 
@@ -13,14 +15,16 @@ def hello():
 def question_classifier():
     j = request.json
     text = j["text"]
-    return {"is_it_a_query": is_it_a_query(text)}
+    return json.dumps({"is_it_a_query": is_it_a_query(text)})
 
 
-@app.route("/extract_todo", methods=["POST"])
+@app.route("/todo_classifier", methods=["POST"])
 def extract_todo():
     j = request.json
     text = j["text"]
-    return {"is_it_a_query": is_it_a_query(text)}
+    category = classify_todo(text)
+    result = {"category": category}
+    return json.dumps(result)
 
 
 if __name__ == "__main__":
