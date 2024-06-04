ecb9ace029242bb1cb6c40719e60c252be6cafd7
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-06-04 14:56:29 +0200

    [todo_classifier] use "plus" dataset version

--- src/py/todo_classifier/train_classifier.py
+++ src/py/todo_classifier/train_classifier.py
@@ -2,13 +2,9 @@ from datasets import load_dataset
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pickle
-from use_classifier import target_names
 
-dataset = load_dataset("clinc_oos", "imbalanced")
+dataset = load_dataset("clinc_oos", "plus")
 train, test, validation = dataset["train"], dataset["test"], dataset["validation"]
 
 model = make_pipeline(TfidfVectorizer(), MultinomialNB())
@@ -21,6 +17,8 @@ with open("model.pkl", "wb") as f:
 
 test_data, test_target = test["text"], test["intent"]
 labels = model.predict(test_data)
+
+print(f"Accuracy: {100 * sum(test_target == labels) / len(test_target):.2f}%")
 
 # TODO: conf matrix only shopping_list, shopping_list update, todo_list, todo_list update vs everything else altogether
 
